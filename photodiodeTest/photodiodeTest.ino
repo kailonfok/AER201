5888a65fac5317e4bed50d26f1b50aea65d6d84d
kailonfok <dknight_77@hotmail.com>
2015-02-02 15:49:44 -0500

    Test code for photodiodes modified
    V1.1
    
    Downloaded code to see PWM possibilities - useless

--- photodiodeTest/photodiodeTest.ino
+++ photodiodeTest/photodiodeTest.ino
@@ -1,16 +1,33 @@
 int analogInPin = 4;
 int circuitReading;
+int previousReading = 0;
+int counter = 0;
+
 
 void setup()
 {
-  Serial.begin(4800);
+  Serial.begin(2400);
   pinMode(analogInPin, INPUT);
 }
 
 void loop()
 {
   circuitReading = analogRead(analogInPin);
+//  if(previousReading > 10)
+//  {
+//    if(circuitReading < 10)
+//    {
+//      counter++;
+//    }
+//  }  
+  if (circuitReading == 0)
+    counter++
+  previousReading = circuitReading;    
   
   Serial.print("The reading is: ");
   Serial.println(circuitReading);
+  Serial.print("The count is: ");
+  Serial.println(counter);
+  
+  //delay(50);
 }
