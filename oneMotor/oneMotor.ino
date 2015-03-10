554294a0866600e977a5e8bc7a384da66b100955
kailonfok <dknight_77@hotmail.com>
2015-03-10 10:11:44 -0400

    Add comments for usability

--- oneMotor/oneMotor.ino
+++ oneMotor/oneMotor.ino
@@ -1,19 +1,19 @@
 #include <AFMotor.h>
 
-AF_DCMotor leftMotor(2);
+AF_DCMotor leftMotor(2); // change the numbers here to "connect" to different motors (labeled on the motor shield)
 AF_DCMotor rightMotor(4);
 
 void setup()
 {
-  leftMotor.setSpeed(180);
+  leftMotor.setSpeed(180); // change these numbers to get varying speed, max is 255
   rightMotor.setSpeed(255);
 
-  leftMotor.run(RELEASE);
+  leftMotor.run(RELEASE); // RELEASE is to brake/stop, don't change these ones
   rightMotor.run(RELEASE);   
 }
 
 void  loop()
 {
-  leftMotor.run(BACKWARD);
+  leftMotor.run(BACKWARD); // change these to change from forward/back, left/right
   rightMotor.run(BACKWARD);
 }
